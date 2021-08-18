5f118567fd5233f21a6db0a651d656ed791d2662
Mihai Morariu <mihaimorariu@gmail.com>
2021-08-18 11:19:29 +0900

    Fix Sphinx errors.

--- pynif3d/loss/eikonal_loss.py
+++ pynif3d/loss/eikonal_loss.py
@@ -6,6 +6,7 @@ from pynif3d.common.verification import check_true
 def eikonal_loss(x):
     """
     Computes the eikonal loss for a given set of points.
+
     Args:
         x (torch.Tensor): Tensor containing the point coordinates. Its shape is
             ``(batch_size, n_samples, 3)`` or ``(n_samples, 3)``.
